921e05286e5676032205816927a4779231da3c56
d3bz <denilsonpersaud@gmail.com>
2026-06-21 10:24:48 -0400

    Edited homedir lines

--- v1_acts_md.py
+++ v1_acts_md.py
@@ -1,12 +1,14 @@
-from pathlib import WindowsPath
+from pathlib import WindowsPath, Path
 from os import listdir, rename
 from docling.document_converter import DocumentConverter
 
 converter = DocumentConverter()
-folder_path = WindowsPath('c:/', 'Users', 'denny','Desktop','Library','Projects','Constitution RAG','Acts Storage') 
+home_dir = Path.home()
+project_path = home_dir / 'Desktop' / 'Library' / 'Projects' / 'Constitution RAG'
+folder_path = project_path / 'Acts Storage'
 pdfs = listdir(path=folder_path)    
 for pdf in range(len(pdfs)):
-    source = WindowsPath('c:/', 'Users', 'denny','Desktop','Library','Projects','Constitution RAG','Acts Storage',pdfs[pdf])
+    source = folder_path / pdfs[pdf]
     doc = converter.convert(source).document
     markdown_output = doc.export_to_markdown()
     with open(f"{pdfs[pdf].replace(".pdf",".md")}", "w", encoding="utf-8") as f:
