764367cbd8fbe7e57fc9a101cd16be9faadb1bc3
d3bz <denilsonpersaud@gmail.com>
2026-06-21 10:32:41 -0400

    Edited more homedir lines

--- v1_read_acts.py
+++ v1_read_acts.py
@@ -1,7 +1,9 @@
 import pandas as pd 
-from pathlib import WindowsPath
+from pathlib import WindowsPath, Path
 
-csv_path = WindowsPath('c:/', 'Users', 'denny','Desktop','Library','Projects','Constitution RAG', 'acts.csv')
+home_dir = Path.home()
+project_path = home_dir / 'Desktop' / 'Library' / 'Projects' / 'Constitution RAG'
+csv_path = project_path / 'Acts Storage' / 'csv' / 'act.csv'
 csv = pd.read_csv(csv_path)
 csv.act_chapter = csv.act_chapter.map(lambda _: _.replace(":",".").replace("A",""))
 csv.act_chapter = csv.act_chapter.astype("float64")
@@ -9,5 +11,5 @@ csv = csv.sort_values(by=["act_chapter"], ascending=True)
 csv.act_chapter = csv.act_chapter.astype("str")
 csv.act_chapter = csv.act_chapter.map(lambda _: _.replace(".",":"))
 csv.act_chapter = csv.act_chapter.map(lambda _: _.replace(":1",":10") if _.endswith(":1") else _)
-csv_path = WindowsPath('c:/', 'Users', 'denny','Desktop','Library','Projects','Constitution RAG', 'updated_acts.csv')
+csv_path = project_path / 'Acts Storage' / 'csv' / 'updated_acts.csv'
 csv.to_csv(path_or_buf=csv_path, index=False,quoting=1)
